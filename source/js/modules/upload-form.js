92f5b87dd95c952beac4b30f4f736b0d99fb0a10
Boris Larchenko <borislarchenko@gmail.com>
2018-12-11 13:27:36 +0200

    start to work with upload-form

---
+++ source/js/modules/upload-form.js
@@ -0,0 +1,19 @@
+(function () {
+  /*In this function we add evt listener to #upload-image input
+  * on change and should show one big picteure for upload
+  * for now we are playing only with one big picture that is in
+  * class .upload-overlay
+  * 1. So we need to find #upload-image input
+  * 2. We need to add change event listener on that input field
+  * This event should show .upload-overlay (delete .hidden class)
+  * 3. We need to find .upload-overlay class to delete its
+  * .hidden class after event
+  * 4. We need to back .hidden class to .upload-overlay after
+  * pressing #upload-cancel button
+  * 5. we need to find #upload-cancel element.
+  * 6. add evet listener evt click on #upload-cancel to add
+  * .hidden class to .upload-overlay
+  * */
+
+
+})();
