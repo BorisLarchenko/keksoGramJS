6e61b0c2ec97b20c4a35d2f64b4c3d7a299efcf6
Boris Larchenko <borislarchenko@gmail.com>
2019-01-21 14:18:25 +0200

    add new method mapFunction that works with array mapping to global window.utils object

--- source/js/modules/utils.js
+++ source/js/modules/utils.js
@@ -1,3 +1,27 @@
+(function(){
+  window.utils ={
+    isEsc: (function () {
+      var ESC_KEYCODE = 27;
+
+      return {
+        isEscKeyCode: function (evt, action) {
+          if (evt.keyCode == ESC_KEYCODE) {
+            action();
+          }
+        }
+
+      }
+    })(),
+    mapFunction: function (it, index, arr) {
+      console.log(it);
+      console.log(index);
+      return it + 1;
+    }
+  }
+})();
+
+
+/*
 window.utils = (function () {
   var ESC_KEYCODE = 27;
 
@@ -9,4 +33,4 @@ window.utils = (function () {
     }
 
   }
-})();
+})();*/
