d9c74c566d533fd49fa2acdc040cefae7d4d562b
Boris Larchenko <borislarchenko@gmail.com>
2019-02-03 20:52:42 +0200

    add other filters

--- source/js/modules/filters.js
+++ source/js/modules/filters.js
@@ -8,6 +8,10 @@
   }
 
   var mostLiked = document.querySelector("label[for=filter-popular]");
+  var mostCommented = document.querySelector("label[for=filter-discussed]");
+  var recommended = document.querySelector("label[for=filter-recommend]");
+  var random = document.querySelector("label[for=filter-random");
+
 
 
 
@@ -32,8 +36,68 @@
 
     window.getDataFromServer.getData('https://js.dump.academy/kekstagram/data', onError, onSucces);
   };
+  var showMostCommentedPic = function () {
+    function onSucces(data) {
+      var startObj = data;
+      var mostCommentObj = startObj.sort(function (obj1, obj2) {
+        return obj2.comments.length - obj1.comments.length;
+      });
+      console.log(mostCommentObj);
+      console.log(startObj);
+
+      //+  delete all little pictures
+      deleteLittlepic();
+      /*- delete all little pictures*/
+      window.utils.render(mostCommentObj);
+
+    }
+    var onError = function(errorMessage){
+      console.log(errorMessage);
+    };
+
+    window.getDataFromServer.getData('https://js.dump.academy/kekstagram/data', onError, onSucces);
+  };
+  var showRecommendedPic = function () {
+    function onSucces(data) {
+
+      //+  delete all little pictures
+      deleteLittlepic();
+      /*- delete all little pictures*/
+      window.utils.render(data);
+
+    }
+    var onError = function(errorMessage){
+      console.log(errorMessage);
+    };
+
+    window.getDataFromServer.getData('https://js.dump.academy/kekstagram/data', onError, onSucces);
+  };
+  var showRandomPic10 = function () {
+    function onSucces(data) {
+      var randomObj = [];
+      for (var i=1; i<= 10; i++) {
+        var newRandomElem = data[Math.floor(Math.random()*data.length)];
+        randomObj.push(newRandomElem);
+      }
+      //+  delete all little pictures
+      deleteLittlepic();
+      /*- delete all little pictures*/
+      window.utils.render(randomObj);
+
+    }
+    var onError = function(errorMessage){
+      console.log(errorMessage);
+    };
+
+    window.getDataFromServer.getData('https://js.dump.academy/kekstagram/data', onError, onSucces);
+  };
+
+
 
   mostLiked.addEventListener('click', showMostLikedPic);
+  mostCommented.addEventListener('click', showMostCommentedPic);
+  recommended.addEventListener('click', showRecommendedPic);
+  random.addEventListener('click', showRandomPic10);
 
 
 })();
