0afe59ab115cfce6d37bdd9df5047332aab55654
Boris Larchenko <borislarchenko@gmail.com>
2019-02-03 20:11:04 +0200

    add special function to delete all pictures in galary

--- source/js/modules/filters.js
+++ source/js/modules/filters.js
@@ -1,4 +1,12 @@
 (function filters() {
+  //+  function to delete all little pictures
+  function deleteLittlepic() {
+    var myContaner = document.querySelector(".pictures");
+    while (myContaner.firstChild) {
+      myContaner.removeChild(myContaner.firstChild);
+    }
+  }
+
   var mostLiked = document.querySelector("label[for=filter-popular]");
 
 
@@ -6,7 +14,6 @@
   var showMostLikedPic = function () {
     function onSucces(data) {
       var startObj = data;
-
       var mostLikedObj = startObj.sort(function (obj1, obj2) {
         return obj2.likes - obj1.likes;
       });
@@ -14,10 +21,7 @@
       console.log(startObj);
 
       //+  delete all little pictures
-      var myContaner = document.querySelector(".pictures");
-      while (myContaner.firstChild) {
-        myContaner.removeChild(myContaner.firstChild);
-      }
+      deleteLittlepic();
       /*- delete all little pictures*/
       window.utils.render(mostLikedObj);
 
